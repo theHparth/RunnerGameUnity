eb0e720b2ad3b00db628d83174561fd6045d3458
piperVLx <pphirpara@gmail.com>
2022-03-30 20:39:33 +0530

    changes

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class WorldGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject ground;
+
+    public Transform player;
     void Start()
     {
-        
+        Instantiate(ground, new Vector3(0,-1.5f,-2.6f), Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    public void GenerateWorld(Vector3 startpoint)
+    {
+        Instantiate(ground, startpoint, Quaternion.identity);
     }
 }
